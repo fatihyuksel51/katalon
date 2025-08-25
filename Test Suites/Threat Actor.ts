0caf18901204ede5c2e6b232461cee6a261a0ed8
Fatih Yuksel <fatih.yuksel@catchprobe.com>
2025-08-25 16:22:47 +0300

    Riskroute opessddddn

--- Test Suites/Threat Actor.ts
+++ Test Suites/Threat Actor.ts
@@ -5,17 +5,31 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>3</numberOfRerun>
+   <numberOfRerun>2</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>0dc99e02-4538-474e-abb7-011b29e503bb</testSuiteGuid>
    <testCaseLink>
-      <guid>e7f17d2f-d486-45d7-94b5-bcd38ff13637</guid>
+      <guid>c10f4644-a07d-4d3d-aeb3-6a64e16ec5bb</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Threatway Test Cases/Threatway</testCaseId>
+      <testCaseId>Test Cases/Threatway Test Cases/TC_OpenAndLogin</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>73e0a5ed-764e-453f-918f-f5c47efe7c3e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Threatway Test Cases/Threatway-Dashboard</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fa33f1b6-2155-43cd-b905-5f67048071bf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Threatway Test Cases/Channel Management</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
