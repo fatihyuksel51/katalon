3d2993d3ad8c19fab9d3902be1e0e6e7aa4bce4a
FATIH-YUKSEL <fatih.yuksel@catchprobe.com>
2025-09-17 13:19:19 +0300

    tesdere

--- Test Suites/PLatform.ts
+++ Test Suites/PLatform.ts
@@ -18,4 +18,32 @@
       <testCaseId>Test Cases/Smartdeceptive Test Case/Attack Flow</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
+   <testCaseLink>
+      <guid>729fd123-6e52-45b5-b0d2-ad3967d9577b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smartdeceptive Test Case/Malware Analyses</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>107cf0dc-0cd7-4109-a7ea-fff0a1b02d2c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smartdeceptive Test Case/Password Attemts</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b1c4e4a1-426b-4459-8c46-03e505fdc67f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smartdeceptive Test Case/Request Records</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>88a47c6f-ef81-44b1-bd9f-d7b9d641b2f7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smartdeceptive Test Case/Structured Attack</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
